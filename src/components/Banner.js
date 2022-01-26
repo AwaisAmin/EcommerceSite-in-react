e097f18c0743ee7a609382a329a24bf9f11a08bf
AwaisAmin <73519531+AwaisAmin@users.noreply.github.com>
2022-01-26 22:02:36 +0500

    Add files via upload

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Banner = () => {
+    return(
+        <div className="container">
+            <header style={{marginTop: '70px'}}>
+                <div className="headerText">
+                    <div>
+                        <h1>Welcome to Pakistan biggest Ecommerce Store</h1>
+                        <h3>Free Delivery All over Pakistan</h3>
+                        <p>Enjoy online shopping</p>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ src/components/Cart.js
@@ -0,0 +1,51 @@
+import React, {useContext} from "react"
+import {cartContext} from "../Global/cartContext"
+
+const Cart = (props) => {
+   
+    const {dispatch, shoppingCart, totalPrice, qty} = useContext(cartContext);
+         console.log("total qty: ",qty);
+    
+    const handleClick = () => {
+        alert('Thanks for shopping')
+    }
+   
+    return(
+       <div className="cartContainer">
+            <div className="cartDetails">
+                {shoppingCart.length ? shoppingCart.map(product => (
+                <div className="cart" key={product.id}>
+                    <span className="cartProImage"><img src={product.image} alt=""/>
+                        <span className="imageCount">{product.qty}</span>
+                    </span>
+                    <span className="cartProductName">{product.name}</span>
+                    <span className="cartProductPrice">${product.price}.00</span>
+                    <span className="inc" onClick={() => dispatch({type: 'INC', id:product.id})}><i className="fas fa-plus"></i></span>
+                    <span className="productQuantity">{product.qty}</span>
+                    <span className="dec" onClick={() => dispatch({type: 'DEC', id: product.id})}><i className="fas fa-minus"></i></span>
+                    <span className="productTotalPrice">${product.qty * product.price}.00</span>
+                    <button onClick={() => dispatch({type: 'DELETE_PRODUCT', id: product.id})} className="deleteCartPro"><i className="fas fa-trash-alt"></i></button>
+                </div>  
+                )) : 'Your Cart is empty!'}     
+            </div>
+            <div className="cartSummary">
+                <div className="summary">
+                    <h3>Order Summary</h3>
+                    <div className="totalItems">
+                        <div className="items">Total Items</div>
+                        <div className="itemsCount">{qty}.00</div>
+                    </div>
+                    <div className="totalPriceSection">
+                        <div className="justTitle">Total Price</div>
+                        <div className="itemsPrice">${totalPrice}.00</div>
+                    </div>
+                    <div className="checkOutSection">
+                        <button className="checkOutbtn" onClick={handleClick}>Check Out</button>
+                    </div>
+                </div>
+            </div>     
+        </div>
+    )
+}
+
+export default Cart;
